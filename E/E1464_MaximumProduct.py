9cfce8976d5c9493026d9aeaa307bc1d6e3acbbf
Andrii Cherniak <theandriicherniak@gmail.com>
2023-04-07 13:42:08 -0700

    Maximum Product of Two Elements in an Array: speedup

--- E/E1464_MaximumProduct.py
+++ E/E1464_MaximumProduct.py
@@ -1,7 +1,21 @@
 from typing import List
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) < 2:
+        L = len(nums)
+        if L < 2:
             return 0
-        nums = sorted(nums)
-        return (nums[-1] - 1) * (nums[-2] - 1)
+        i = 0
+        j = L-1
+        result = (nums[i] - 1) * (nums[j] - 1)
+        if nums[i] <= nums[j]:
+            i += 1
+        else:
+            j -= 1
+        while i < j:
+            r = (nums[i] - 1) * (nums[j] - 1)
+            result = max(r, result)
+            if nums[i] <= nums[j]:
+                i += 1
+            else:
+                j -= 1
+        return result
