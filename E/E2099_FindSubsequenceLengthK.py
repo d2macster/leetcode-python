9099f7521830bd4ccca6c7460954751e4ebf0bca
Andrii Cherniak <theandriicherniak@gmail.com>
2023-04-07 14:54:56 -0700

    Find Subsequence of Length K With the Largest Sum

---
+++ E/E2099_FindSubsequenceLengthK.py
@@ -0,0 +1,30 @@
+from typing import List
+from collections import defaultdict
+import heapq
+class Solution:
+    def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
+        q = []
+        lq = 0
+        positions = defaultdict(list)
+        for nid, n in enumerate(nums):
+            positions[n].append(nid)
+            heapq.heappush(q, n)
+            lq += 1
+            if lq > k:
+                np = heapq.heappop(q)
+                lq -= 1
+                positions[np].pop(0)
+
+        survivors = []
+        for n, nids in positions.items():
+            if not nids:
+                continue
+            for nid in nids:
+                survivors.append((n, nid))
+        survivors = sorted(survivors, key=lambda t: t[1])
+        return [s[0] for s in survivors]
+    
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maxSubsequence([2,1,3,3],2))
+    print(s.maxSubsequence([-1,-2,3,4], 3))
