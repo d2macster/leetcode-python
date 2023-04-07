fe8f43f14ab6b4d5c4e97c37746d9f48c2063e0d
Andrii Cherniak <theandriicherniak@gmail.com>
2023-04-07 15:23:54 -0700

    heap-only Find Subsequence of Length K With the Largest Sum

--- E/E2099_FindSubsequenceLengthK.py
+++ E/E2099_FindSubsequenceLengthK.py
@@ -5,24 +5,18 @@ class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
         q = []
         lq = 0
-        positions = defaultdict(list)
         for nid, n in enumerate(nums):
-            positions[n].append(nid)
-            heapq.heappush(q, n)
+            heapq.heappush(q, (n, nid))
             lq += 1
             if lq > k:
-                np = heapq.heappop(q)
+                heapq.heappop(q)
                 lq -= 1
-                positions[np].pop(0)
 
-        survivors = []
-        for n in q:
-            nid = positions[n].pop(0)
-            survivors.append((n, nid))
-        survivors = sorted(survivors, key=lambda t: t[1])
-        return [s[0] for s in survivors]
+        q = sorted(q, key=lambda t: t[1])
+        return [e[0] for e in q]
     
 if __name__ == "__main__":
     s = Solution()
     print(s.maxSubsequence([2,1,3,3], 2))
     print(s.maxSubsequence([-1,-2,3,4], 3))
+    print(s.maxSubsequence([3,4,3,3], 2))
