2aa3908c20a48342a24e0fd37f6cbeff46a724b4
Andrii Cherniak <theandriicherniak@gmail.com>
2023-04-08 17:54:23 -0700

    speedup Maximum Product Difference Between Two Pairs

--- E/E1913_MaximumProductDifference.py
+++ E/E1913_MaximumProductDifference.py
@@ -1,5 +1,30 @@
 from typing import List
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        return nums[-1] * nums[-2] - nums[0] * nums[1]
+        max1 = nums[0]
+        max2 = nums[1]
+        if max2 > max1:
+            max1, max2 = max2, max1
+        
+        for n in nums[2:]:
+            if n <= max2:
+                continue
+            max2 = max1
+            max1 = n
+            if max2 > max1:
+                max1, max2 = max2, max1
+
+        min1 = nums[0]
+        min2 = nums[1]
+        if min2 < min1:
+            min1, min2 = min2, min1
+
+        for n in nums[2:]:
+            if n >= min2:
+                continue
+            min2 = min1
+            min1 = n
+            if min2 < min1:
+                min1, min2 = min2, min1
+
+        return max1 * max2 - min1 * min2
