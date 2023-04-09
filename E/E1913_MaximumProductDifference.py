c8bdd6d0a39abfea731ee84de2b60aa61c27a17d
Andrii Cherniak <theandriicherniak@gmail.com>
2023-04-08 17:41:52 -0700

    Maximum Product Difference Between Two Pairs

---
+++ E/E1913_MaximumProductDifference.py
@@ -0,0 +1,5 @@
+from typing import List
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums = sorted(nums)
+        return nums[-1] * nums[-2] - nums[0] * nums[1]
