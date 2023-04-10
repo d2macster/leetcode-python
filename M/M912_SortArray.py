1d179dc436c0800532dc29a68a28ca63ee541ccd
Andrii Cherniak <theandriicherniak@gmail.com>
2023-04-10 11:13:26 -0700

    merge sort version for Sort an Array

--- M/M912_SortArray.py
+++ M/M912_SortArray.py
@@ -1,16 +1,41 @@
 from typing import List
-from collections import defaultdict
-class Solution:
+class Solution:        
     def sortArray(self, nums: List[int]) -> List[int]:
         L = len(nums)
         if L <= 1:
             return nums
-        counter = defaultdict(int)
-        for n in nums:
-            counter[n] += 1
+        if L == 2:
+            if nums[0] > nums[1]:
+                nums[0], nums[1] = nums[1], nums[0]
+            return nums
+        result = [0 for _ in range(L)]
+        l2 = int(L/2)
+        n1 = self.sortArray(nums[0:l2])
+        n2 = self.sortArray(nums[l2:])
         i = 0
-        for k in sorted(counter.keys()):
-            for _ in range(counter[k]):
-                nums[i] = k
+        i1 = 0
+        i2 = 0
+        l1 = len(n1)
+        l2 = len(n2)
+        while i1 < l1 or i2 < l2:
+            if i1 == l1:
+                result[i] = n2[i2]
+                i2 += 1
                 i += 1
-        return nums
+                continue
+            if i2 == l2:
+                result[i] = n1[i1]
+                i1 += 1
+                i += 1
+                continue
+            if n1[i1] <= n2[i2]:
+                result[i] = n1[i1]
+                i += 1
+                i1 += 1
+                continue
+            else:
+                result[i] = n2[i2]
+                i += 1
+                i2 += 1
+
+        return result
