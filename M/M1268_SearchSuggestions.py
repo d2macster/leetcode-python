0b75ed759b559e3180fb2c1c1d7ed63e2b82a511
Andrii Cherniak <theandriicherniak@gmail.com>
2023-04-12 11:24:26 -0700

    Search Suggestions System

--- M/M1268_SearchSuggestions.py
+++ M/M1268_SearchSuggestions.py
@@ -1,4 +1,5 @@
 from typing import List
+import bisect
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
@@ -6,12 +7,13 @@ class Solution:
         if Ls == 0:
             return []
         result = []
+        products = sorted(products)
         for i in range(Ls):
             prefix = searchWord[0:i+1]
-            cache = []
-            for p in products:
-                if not p.startswith(prefix):
-                    continue
-                cache.append(p)
-            result.append(sorted(cache)[0:3])
+            pi = bisect.bisect_left(products, prefix)
+            result.append([p for p in products[pi:pi+3] if p.startswith(prefix)])
         return result
+    
+if __name__ == "__main__":
+    s = Solution()
+    s.suggestedProducts(["mobile","mouse","moneypot","monitor","mousepad"], "mouse")
