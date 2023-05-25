4b34f5c581c6a416a0f5596306432e9e8ae62b57
Andrii Cherniak <theandriicherniak@gmail.com>
2023-05-25 12:36:31 -0700

    Sort Colors via pointers

--- M/M75_SortColors.py
+++ M/M75_SortColors.py
@@ -1,11 +1,43 @@
 from typing import List
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        counter = [0, 0, 0]
-        for n in nums:
-            counter[n] += 1
+        L = len(nums)
+        if L < 2:
+            return
+        i0 = 0
+        i2 = L-1
         i = 0
-        for k in [0, 1, 2]:
-            for j in range(counter[k]):
-                nums[i + j] = k
-            i += counter[k]
+        while i <= i2:
+            while i0 <= i2 and nums[i0] == 0:
+                i0 += 1
+                i = i0
+            while i2 >= i0 and nums[i2] == 2:
+                i2 -= 1
+            if i > i2:
+                break
+            if nums[i] == 1:
+                i+= 1
+                continue
+            if nums[i] == 2:
+                nums[i] = nums[i2]
+                nums[i2] = 2
+                i2 -= 1
+                continue
+            if nums[i] == 0:
+                nums[i] = nums[i0]
+                nums[i0] = 0
+                i0 += 1
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [2,0,2,1,1,0]
+    s.sortColors(nums)
+    print(nums)
+    nums = [2,0,1]
+    s.sortColors(nums)
+    print(nums)
+
+    nums = [0,0]
+    s.sortColors(nums)
+    print(nums)
